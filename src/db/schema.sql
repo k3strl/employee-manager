1bdea4f3e5630a82aa5d2e75dff2d3c6cdbb61f6
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2024-11-12 15:55:31 -0600

    added SQL code to schema.sql

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -3,40 +3,24 @@
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
-\c library_db
+\c employees_db
 
-CREATE TABLE 
+CREATE TABLE department(
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(30) UNIQUE NOT NULL
+)
 
+CREATE TABLE role(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL
+)
 
-
--- department
-
--- id: SERIAL PRIMARY KEY
-
--- name: VARCHAR(30) UNIQUE NOT NULL to hold department name
-
-
-
--- role
-
--- id: SERIAL PRIMARY KEY
-
--- title: VARCHAR(30) UNIQUE NOT NULL to hold role title
-
--- salary: DECIMAL NOT NULL to hold role salary
-
--- department_id: INTEGER NOT NULL to hold reference to department role belongs to
-
-
-
--- employee
-
--- id: SERIAL PRIMARY KEY
-
--- first_name: VARCHAR(30) NOT NULL to hold employee first name
-
--- last_name: VARCHAR(30) NOT NULL to hold employee last name
-
--- role_id: INTEGER NOT NULL to hold reference to employee role
-
--- manager_id: INTEGER to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+CREATE TABLE employee(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER
+)
